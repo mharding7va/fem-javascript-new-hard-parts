8948501694aea4958dc8eb2bb514a6d362a511b7
Michael Harding <mharding@genesisdla.com>
2018-09-16 19:56:43 -0400

    Iterators with closure

---
+++ 3-promises/promises.js
@@ -0,0 +1,11 @@
+// these functions are for learning only. they DO NOT WORK!
+
+function display(data) {
+    console.log(data);
+}
+
+const futureData = fetch("http://twitter/will/1");
+
+futureData.then(display);
+
+console.log("Me First!");
---
+++ 4-iterators/iterators.js
@@ -0,0 +1,33 @@
+function createNewFunction() {
+    function add2 (num) {
+        return num + 2;
+    }
+    return add2
+}
+
+const newFunction = createNewFunction();
+
+const result = newFunction(3);
+
+// -----------------------
+// COVE or Closure
+
+function createFunction(array) {
+    let i = 0;
+    function inner() {
+        const element = array[i];
+        i++;
+        return element;
+    }
+    return inner;
+}
+
+const returnNextElement = createFunction([4, 5, 6]);
+
+const element1 = returnNextElement();
+const element2 = returnNextElement();
+const element3 = returnNextElement();
+
+console.log(element1);
+console.log(element2);
+console.log(element3);
